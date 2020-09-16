2d8dc9177d766a88b7d4a43d0dac680fd787876a
Mark Yakovlev <yakovlev.mark96@gmail.com>
2020-09-16 19:12:07 +0300

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Mark Yakovlev on 16.09.2020.
+//  Copyright © 2020 Mark Yakovlev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
